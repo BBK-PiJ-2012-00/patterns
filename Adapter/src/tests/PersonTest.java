1628a77f2dc14dd77702a8cd6410f11565d408a9
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-26 14:59:59 +0000

    Testing

--- Adapter/src/tests/PersonTest.java
+++ Adapter/src/tests/PersonTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import example.AnotherPerson;
 import example.Nosrep;
+import example.PAP;
 import example.Person;
 
 public class PersonTest {
@@ -29,6 +30,18 @@ public class PersonTest {
 		actualAnswer = classUnderTest.youAreWho(); 
 		assertEquals("Wrong Answer!", expectedAnswer, actualAnswer);
 	}
+	
+	
+	@Test
+	public void test3() {
+		String expectedAnswer = "A person";
+		String actualAnswer;
+		
+		PAP classUnderTest = new PAP();
+		actualAnswer = classUnderTest.youAreWho();
+		assertEquals("Wrong Answer!", expectedAnswer, actualAnswer);
+	}
 
 }
+
 
