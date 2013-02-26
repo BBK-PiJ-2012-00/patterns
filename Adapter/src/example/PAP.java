51281a3336c43b4446e75719937bf4e6ad45470a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-26 14:59:37 +0000

    Added PAP

---
+++ Adapter/src/example/PAP.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package example;
+
+/**
+ * @author ataylo20
+ *
+ */
+public class PAP extends Person implements AnotherPerson {
+
+	/* (non-Javadoc)
+	 * @see example.AnotherPerson#youAreWho()
+	 */
+	@Override
+	public String youAreWho() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
